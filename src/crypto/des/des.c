2a7e69d15674dab5a734a31d2e7f9062de20ab3d
faiaz <fayaz.sanaulla@gmail.com>
2017-03-19 15:13:44 +0200

    des

--- src/crypto/des/des.c
+++ src/crypto/des/des.c
@@ -1,4 +1,20 @@
+void encode_des(char *str, char *key);
+int permutation(int n);
 
 int main {
     return 0;
 };
+
+void encode_des(char *str, char *key) {
+
+}
+
+int permutation(int n) {
+    return n;
+}
+
+int expansion(int n) {
+    return n;
+}
+
+void s_box(int n) {}
